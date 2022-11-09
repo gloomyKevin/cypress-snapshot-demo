23516bc54f428febae1de959c3b7e00777ed2d47
wanyixuan <wanyixuan@baidu.com>
2022-11-09 22:07:53 +0800

    init cypress-plugin-visual-regression-diff demo

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,24 @@
 const { defineConfig } = require('cypress')
-const { initPlugin } = require('cypress-plugin-snapshots/plugin')
+// const { initPlugin } = require('cypress-plugin-snapshots/plugin')
+const { initPlugin } = require('@frsource/cypress-plugin-visual-regression-diff/plugins');
 const { 
   addMatchImageSnapshotPlugin, 
   } = require('cypress-image-snapshot/plugin');
 
 module.exports = defineConfig({
   env: {
-    "cypress-plugin-snapshots": {
-      "imageConfig": {
-        "createDiffImage": true,                // Should a "diff image" be created, can be disabled for performance
-        "threshold": 0.01,                      // Amount in pixels or percentage before snapshot image is invalid
-        "thresholdType": "percent",             // Can be either "pixel" or "percent"
-      },
-      // "name": "custom image name",            // Naming resulting image file with a custom name rather than concatenating test titles
-      // "separator": "custom image separator",  // Naming resulting image file with a custom separator rather than using the default ` #`
-      // "updateSnapshots": true,
-    }
+    // pluginVisualRegressionUpdateImages: true,
+    "pluginVisualRegressionDiffConfig": { threshold: 0.01 },
+    // "cypress-plugin-snapshots": {
+    //   "imageConfig": {
+    //     "createDiffImage": true,                // Should a "diff image" be created, can be disabled for performance
+    //     "threshold": 0.01,                      // Amount in pixels or percentage before snapshot image is invalid
+    //     "thresholdType": "percent",             // Can be either "pixel" or "percent"
+    //   },
+    //   // "name": "custom image name",            // Naming resulting image file with a custom name rather than concatenating test titles
+    //   // "separator": "custom image separator",  // Naming resulting image file with a custom separator rather than using the default ` #`
+    //   // "updateSnapshots": true,
+    // }
   },
   e2e: {
     excludeSpecPattern: [
@@ -25,8 +28,13 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       initPlugin(on, config);
-      addMatchImageSnapshotPlugin(on, config); 
+      // addMatchImageSnapshotPlugin(on, config); 
       return config;
     },
-  }
+  },
+  // component: {
+  //   setupNodeEvents(on, config) {
+  //     initPlugin(on, config);
+  //   }
+  // }
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,18 +23,19 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-import "cypress-plugin-snapshots/commands";
-import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
-addMatchImageSnapshotCommand({ failureThreshold 
-  : 0.00, failureThresholdType 
-  : 'percent', 
-  customDiffConfig: { threshold: 0.0 }, 
-  capture: 'viewport', 
-});
-Cypress.Commands.add("setResolution", (size) => { 
-  if (Cypress._.isArray(size)) { 
-     cy.viewport(size[0], size[1]); 
-   } else { 
-    cy.viewport(size); 
-  } 
-})
+// import "cypress-plugin-snapshots/commands";
+import "@frsource/cypress-plugin-visual-regression-diff";
+// import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
+// addMatchImageSnapshotCommand({ failureThreshold 
+//   : 0.00, failureThresholdType 
+//   : 'percent', 
+//   customDiffConfig: { threshold: 0.0 }, 
+//   capture: 'viewport', 
+// });
+// Cypress.Commands.add("setResolution", (size) => { 
+//   if (Cypress._.isArray(size)) { 
+//      cy.viewport(size[0], size[1]); 
+//    } else { 
+//     cy.viewport(size); 
+//   } 
+// })
